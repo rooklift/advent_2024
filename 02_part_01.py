f657a1132acc4b13c6685fc7e01d73a8b4c59a39
rooklift <16438795+rooklift@users.noreply.github.com>
2024-12-02 19:24:20 +0000

    Update 02_part_01.py

--- 02_part_01.py
+++ 02_part_01.py
@@ -1,10 +1,7 @@
 def parser(filename):
-
 	with open(filename) as infile:
 		rawlines = [line.strip() for line in infile.readlines() if line.strip() != ""]
-
 	token_lists = [line.split() for line in rawlines]
-
 	return [[int(s) for s in l] for l in token_lists]
 
 
@@ -16,22 +13,25 @@ def same_sign(a, b):				# Don't care about cases where either number is zero
 	return False
 
 
+def sequence_is_safe(arr):
+	last_diff = None
+	for i in range(len(arr) - 1):
+		diff = arr[i + 1] - arr[i]
+		if abs(diff) < 1 or abs(diff) > 3:
+			return False
+		if last_diff != None and not same_sign(diff, last_diff):
+			return False
+		last_diff = diff
+	return True
+
+
 def main():
 
 	num_list = parser("02_input.txt")
 
 	count = 0
-
 	for line in num_list:
-		last_diff = None
-		for i in range(len(line) - 1):
-			diff = line[i + 1] - line[i]
-			if abs(diff) < 1 or abs(diff) > 3:
-				break
-			if last_diff != None and not same_sign(diff, last_diff):
-				break
-			last_diff = diff
-		else:						# The "else" is run if the inner for loop is NOT broken.
+		if sequence_is_safe(line):
 			count += 1
 
 	print(count)
