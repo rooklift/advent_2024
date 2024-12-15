f436f16f3a4ba281dc29485f939a08d5156b04a8
rooklift <16438795+rooklift@users.noreply.github.com>
2024-12-15 14:01:32 +0000

    Update 15_part_02.py

--- 15_part_02.py
+++ 15_part_02.py
@@ -80,21 +80,14 @@ def get_moveables_vertical(maze, direction, box_x, box_y):
 
 	items = get_connected_vertical(maze, direction, box_x, box_y)
 
-	ret = []
-
 	for item in items:
-
 		x = item[1]
 		y = item[2]
-
 		new_y = y + vecs[direction][1]
-
 		if maze[x][new_y] == "#":
 			return []
-		else:
-			ret.append(item)
 
-	return ret
+	return list(items)
 
 
 def get_connected_vertical(maze, direction, x, y):
@@ -116,6 +109,9 @@ def get_connected_vertical(maze, direction, x, y):
 		y = item[2]
 
 		new_y = y + vecs[direction][1]
+
+		if (x, new_y) in result:
+			continue
 
 		if maze[x][new_y] in "[]":
 			result.add(this_half(maze, x, new_y))
