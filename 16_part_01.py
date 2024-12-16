743fb25179c11b09ac4418c849fa74ec242e36de
rooklift <16438795+rooklift@users.noreply.github.com>
2024-12-16 12:01:18 +0000

    broken

--- 16_part_01.py
+++ 16_part_01.py
@@ -1,6 +1,7 @@
 import copy
 from PIL import Image
 
+
 def parser(filename):
 	with open(filename) as infile:
 		lines = [line.strip() for line in infile.readlines() if line.strip() != ""]
@@ -40,11 +41,160 @@ def grid_to_png(grid, outfilename):
 
 	img.save(outfilename)
 
+# -------------------------------------------------------------------------------------------------
+
+RIGHT = (1, 0)
+LEFT = (-1, 0)
+UP = (0, -1)
+DOWN = (0, 1)
+
+ALL_DIRECTIONS = [UP, RIGHT, DOWN, LEFT]
+
 
 def main():
+
 	original, startx, starty, endx, endy = parser("16_input.txt")
+	width, height = width_height(original)
+
+	# Remove dead ends from the grid... this simplifies various things.
+
 	grid = anti_dead_end(original, startx, starty, endx, endy)
-	grid_to_png(grid, "16_anti.png")
+
+	# We should construct a set of nodes. Departing an x,y start in 2 different directions
+	# counts as 2 different nodes.
+
+	all_connections = dict()					# startx, starty, (dx, dy)  -->  endx, endy, (dx, dy), cost
+
+	# For each node-start we could also add as connections the cost of turning as a connection...
+
+	possible_states = all_possible_states(grid, startx, starty, endx, endy)
+
+	for state in possible_states:
+		all_connections[state] = get_connection(grid, possible_states, state[0], state[1], state[2])
+
+	print(all_connections)
+
+
+
+
+def get_connection(grid, possible_states, x, y, d):
+
+	cost = 0
+
+	nextx = x + d[0]
+	nexty = y + d[1]
+
+	if grid[nextx][nexty] != ".":
+		return None
+
+	while True:
+
+		# Move...
+
+		x = x + d[0]
+		y = y + d[1]
+		cost += 1
+
+		# Check if we reached a new node...
+
+		if (x, y, d) in possible_states:
+			return (x, y, d, cost)
+
+		# Is it OK to continue moving?
+
+		nextx = x + d[0]
+		nexty = y + d[1]
+
+		if grid[nextx][nexty] != ".":
+
+			# We're either turning left or right.
+
+			cost += 1000
+
+			choices = 0
+
+			for nextd in perpendicular_directions(d):
+				nextx = x + nextd[0]
+				nexty = y + nextd[1]
+				if grid[nextx][nexty] == ".":
+					d = nextd
+					choices += 1
+
+			try:
+				assert(choices == 1)
+			except:
+				print(x, y)
+				raise AssertionError
+
+
+def perpendicular_directions(d):
+	turns = {
+		RIGHT: [UP, DOWN],
+		LEFT: [UP, DOWN],
+		UP: [LEFT, RIGHT],
+		DOWN: [LEFT, RIGHT],
+	}
+	return turns[d]
+
+# -------------------------------------------------------------------------------------------------
+
+def all_possible_states(grid, startx, starty, endx, endy):
+
+	# Returns all possible states the robot can be in, at the moment when it has to make decisions.
+	# Probably this only works if dead ends have been removed.
+
+	width, height = width_height(grid)
+
+	foo = []								# startx, starty, (dx, dy)
+
+	for x in range(width):
+		for y in range(height):
+			if grid[x][y] == ".":
+				dirs_out = directions_out(grid, x, y)
+				if len(dirs_out) > 2:
+					for d in dirs_out:
+						foo.append((x, y, d))
+
+	start_present = False
+	end_present = False
+	for state in foo:
+		if state[0] == startx and state[1] == starty:
+			start_present = True
+		if state[0] == endx and state[1] == endy:
+			end_present = True
+
+	assert(start_present == False and end_present == False)		# This will make life easier.
+
+	# Add start...
+
+	for d in directions_out(grid, startx, starty):
+		foo.append((startx, starty, d))
+
+	if (startx, starty, RIGHT) not in foo:						# Robot starts facing east.
+		foo.append((startx, starty, RIGHT))
+
+	# Add end... note that our robot ends pointing away from a path out, thus the negation...
+
+	for d in directions_out(grid, endx, endy):
+		dx, dy = d
+		foo.append((endx, endy, (-dx, -dy)))
+
+	return foo
+
+# -------------------------------------------------------------------------------------------------
+
+def directions_out(grid, x, y):
+	assert(grid[x][y] == ".")
+	ret = []
+	if grid[x - 1][y] == ".":
+		ret.append(LEFT)
+	if grid[x + 1][y] == ".":
+		ret.append(RIGHT)
+	if grid[x][y - 1] == ".":
+		ret.append(UP)
+	if grid[x][y + 1] == ".":
+		ret.append(DOWN)
+	return ret
 
 
 def neighbours(grid, x, y):
@@ -88,7 +238,7 @@ def anti_dead_end(original, startx, starty, endx, endy):
 		assert(len(all_neighbours) == 1)
 		neighbour = all_neighbours[0]
 
-		grid[x][y] = "O"
+		grid[x][y] = "O"					# FIXME? - dangerous to have 2 types of wall.
 
 		if len(neighbours(grid, neighbour[0], neighbour[1])) == 1:
 			if neighbour not in been_in_queue:
@@ -100,5 +250,6 @@ def anti_dead_end(original, startx, starty, endx, endy):
 
 	return grid
 
+# -------------------------------------------------------------------------------------------------
 
 main()
