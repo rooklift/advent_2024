dc246988074b52ccb4944c56dfa3026506286abd
rooklift <16438795+rooklift@users.noreply.github.com>
2024-12-17 15:35:31 +0000

    Create 17_part_01.py

---
+++ 17_part_01.py
@@ -0,0 +1,97 @@
+def parser(filename):
+	with open(filename) as infile:
+		parts = infile.read().split("\n\n")
+	regs = parts[0].split("\n")
+	a = int(regs[0].split("A:")[1])
+	b = int(regs[1].split("B:")[1])
+	c = int(regs[2].split("C:")[1])
+	program = [int(s) for s in parts[1].split(":")[1].split(",")]
+	return a, b, c, program
+
+
+class Halt(Exception):
+    pass
+
+
+class Computer():
+
+	def __init__(self, a, b, c, program):
+		self.reset(a, b, c, program)
+
+
+	def reset(self, a, b, c, program):
+		self.a = a
+		self.b = b
+		self.c = c
+		self.program = program
+		self.program_string = ",".join([str(n) for n in program])
+		self.i = 0
+		self.outputs = []
+
+
+	def get_output(self):
+		while True:
+			try:
+				self.step()
+			except Halt:
+				return self.outputs
+
+
+	def get_output_string(self):
+		return ",".join([str(n) for n in self.get_output()])
+
+
+	def step(self):
+		if self.i >= len(self.program):
+			raise Halt
+		self.act(self.program[self.i], self.program[self.i + 1])
+
+
+	def act(self, opcode, value):			# Where value is the raw number at i+1
+
+		# Note: operands may be either literals or combos, depending on the opcode.
+
+		next_i = self.i + 2					# usually true
+
+		if opcode == 0:						# adv (division using combo)
+			self.a = self.a // (2 ** self.interpret_combo(value))
+		elif opcode == 1:					# bxl (bitwise xor using literal)
+			self.b = self.b ^ value
+		elif opcode == 2:					# bst (combo modulo 8)
+			self.b = self.interpret_combo(value) % 8
+		elif opcode == 3:					# jnz (conditional jump using literal)
+			if self.a == 0:
+				pass
+			else:
+				next_i = value
+		elif opcode == 4:					# bxc (bitwise xor of a^b, reads operand but ignores it)
+			self.b = self.b ^ self.c
+		elif opcode == 5:					# out
+			self.outputs.append(self.interpret_combo(value) % 8)
+		elif opcode == 6:					# bdv (like adv but storing to b)
+			self.b = self.a // (2 ** self.interpret_combo(value))
+		elif opcode == 7:					# cdv (like adv but storing to c)
+			self.c = self.a // (2 ** self.interpret_combo(value))
+
+		self.i = next_i
+
+
+	def interpret_combo(self, n):
+		if n <= 3:
+			return n
+		if n == 4:
+			return self.a
+		if n == 5:
+			return self.b
+		if n == 6:
+			return self.c
+		if n == 7:
+			raise ValueError
+
+
+def main():
+	computer = Computer(*parser("17_input.txt"))
+	print(computer.get_output_string())
+
+
+main()
