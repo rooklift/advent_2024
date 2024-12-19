0afdf7b51048c9fdadd160f4c4b2fb3d8e962c09
rooklift <16438795+rooklift@users.noreply.github.com>
2024-12-19 12:16:57 +0000

    Create 19_part_both.py

---
+++ 19_part_both.py
@@ -0,0 +1,36 @@
+import functools
+
+def parser(filename):
+	with open(filename) as infile:
+		raw = infile.read()
+	parts = raw.split("\n\n")
+	tokens = parts[0].split(", ")
+	targets = [line.strip() for line in parts[1].split("\n") if line.strip() != ""]
+	return tuple(tokens), targets
+
+
+def main():
+	tokens, targets = parser("19_input.txt")
+	p1 = 0
+	p2 = 0
+	for target in targets:
+		ways = ways_to_build(tokens, target)
+		if ways > 0:
+			p1 += 1
+			p2 += ways
+	print(p1)
+	print(p2)
+
+
+@functools.cache
+def ways_to_build(tokens, target):
+	if len(target) == 0:
+		return 1
+	ret = 0
+	for token in tokens:
+		if target.startswith(token):
+			ret += ways_to_build(tokens, target[len(token):])
+	return ret
+
+
+main()
