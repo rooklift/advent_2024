68a899c2a1e2ab5e07bd142e3e8802e2e4207fb0
rooklift <16438795+rooklift@users.noreply.github.com>
2024-12-21 13:19:51 +0000

    Update 21_part_01.py

--- 21_part_01.py
+++ 21_part_01.py
@@ -2,7 +2,7 @@ def parser(filename):
 	with open(filename) as infile:
 		return [line.strip() for line in infile.readlines() if line.strip() != ""]
 
-big_lines = [		# Note that these arrays have [y][x] format - but we convert them in make_keypad_grid()
+big_lines = [												# These arrays have [y][x] format - convert them in make_keypad_grid()
 	"789",
 	"456",
 	"123",
@@ -15,7 +15,7 @@ small_lines = [
 ]
 
 
-def make_keypad_dict(lines):			# Locations of everything
+def make_keypad_dict(lines):								# Dict: c --> (x, y)
 	ret = dict()
 	for y, line in enumerate(lines):
 		for x, c in enumerate(line):
@@ -31,141 +31,97 @@ vectors = {
 }
 
 
-def next_position(kp_dict, c, move):		# Given our position at c and a movement, what char are we at next?
-
-	assert(move in "><^v")
-
+def next_position(kp_dict, c, move):						# Given our position at c and a movement in "<>^v", what char are we at next?
+	assert(move in "<>^v")
 	x, y = kp_dict[c]
-
-	x += vectors[move][0]
-	y += vectors[move][1]
-
+	dx, dy = vectors[move]
+	x += dx
+	y += dy
 	for key in kp_dict:
 		if kp_dict[key] == (x, y):
 			return key
-
 	raise AssertionError
 
 
-def next_moves(kp_dict, c1, c2):		# Given our position at c1, return all sane next moves if we want to push c2
-
+def next_moves(kp_dict, c1, c2):							# Given our position at c1, return 1 or 2 possible next moves if we want to push c2.
 	assert(c1 != c2)
-
 	x1, y1 = kp_dict[c1]
 	x2, y2 = kp_dict[c2]
-
-	dx = x2 - x1
-	dy = y2 - y1
-
 	ret = []
 	poss = []
-
-	if dx > 0:
+	if x1 < x2:
 		poss.append(">")
-	if dx < 0:
+	if x2 < x1:
 		poss.append("<")
-	if dy > 0:
+	if y1 < y2:
 		poss.append("v")
-	if dy < 0:
+	if y2 < y1:
 		poss.append("^")
-
 	for move in poss:
 		next_c = next_position(kp_dict, c1, move)
 		if next_c != ".":
 			ret.append(move)
-
 	return ret
 
 
-def action_sequences(kp_dict, c1, c2, last_move = None):
-
-	# last_move ensures we don't turn without cause. e.g. [left left up up] is OK, [left up left up] is not.
-
+def action_sequences(kp_dict, c1, c2, last_move = None):	# Given our position at c1, return all sane sequences to go to c2, AND PUSH IT.
 	if c1 == c2:
 		return [["A"]]
-
 	all_next = next_moves(kp_dict, c1, c2)
-
 	if last_move in all_next:
 		all_next = [last_move]
-
 	ret = []
-
 	for move in all_next:
 		next_c = next_position(kp_dict, c1, move)
-
 		for foo in action_sequences(kp_dict, next_c, c2, move):
 			ret.append([move] + foo)
-
 	return ret
 
 
-def full_sequences(kp_dict, c1, buttons):		# Given buttons, with arm at c1, return all sane sequences to press them.
-
+def full_sequences(kp_dict, c1, buttons):					# Given buttons, with arm at c1, return all sane sequences to press them.
 	curr_c = c1
 	next_c = buttons[0]
-
 	ret = action_sequences(kp_dict, curr_c, next_c)
-
 	for button in buttons[1:]:
 		curr_c = next_c
 		next_c = button
 		additional = action_sequences(kp_dict, curr_c, next_c)
-
 		new_ret = []
-
 		for add in additional:
 			for partial in ret:
 				new_ret.append(partial + add)
-
 		ret = new_ret
-
 	return ret
 
 
-def score(s, push_count):
+def score(s, push_count):									# Score for code s given how many buttons the human presses.
 	return int(s[0:3]) * push_count
 
 
-def main():
-
-	codes = parser("21_input.txt")
-
-	big = make_keypad_dict(big_lines)
-	small = make_keypad_dict(small_lines)
-
+def solve_code(big_kp_dict, small_kp_dict, code, intermediate_robots):
 	result = 0
+	seqs = full_sequences(big_kp_dict, "A", code)
+	for n in range(intermediate_robots):
+		next_level = []
+		for seq in seqs:
+			next_level += full_sequences(small_kp_dict, "A", seq)
+		seqs = next_level
+	min_buttons = None
+	for seq in seqs:
+		if min_buttons == None or len(seq) < min_buttons:
+			min_buttons = len(seq)
+	return score(code, min_buttons)
 
+
+def main():
+	codes = parser("21_input.txt")
+	big_kp_dict = make_keypad_dict(big_lines)
+	small_kp_dict = make_keypad_dict(small_lines)
+	result = 0
 	for code in codes:
-
 		print(code)
-
-		robot_1_seqs = full_sequences(big, "A", code)
-
-		robot_2_seqs = []
-
-		for r1seq in robot_1_seqs:
-			robot_2_seqs += full_sequences(small, "A", r1seq)
-
-		robot_3_seqs = []
-
-		for r2seq in robot_2_seqs:
-			robot_3_seqs += full_sequences(small, "A", r2seq)
-
-		min_buttons = None
-
-		for seq in robot_3_seqs:
-			if min_buttons == None or len(seq) < min_buttons:
-				min_buttons = len(seq)
-
-		result += score(code, min_buttons)
-
+		result += solve_code(big_kp_dict, small_kp_dict, code, 2)
 	print(result)
-
-
-
-
-
 
 
 main()
